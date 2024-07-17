198da09d32eabbf10f86b5d5676f1af67104e976
Neto Russo <netoacrn1@gmail.com>
2024-07-17 12:52:09 -0300

    Subindo o projeto base da Turnover

---
+++ src/main/java/br/com/turnover/TurnoverApplication.java
@@ -0,0 +1,13 @@
+package br.com.turnover;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurnoverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurnoverApplication.class, args);
+	}
+
+}
