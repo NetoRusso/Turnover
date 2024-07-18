1d92b6539799895c63952e7ed6faa2714a91afec
Pedro Augusto Montes <nerdindiano@gmail.com>
2024-07-18 12:14:15 -0300

    Adicionado Lombok e a anotacao @Data para adicionar automaticamente os boiler plate getters, setters e construtores das entidades

--- src/main/java/br/com/turnover/models/DepartamentoModel.java
+++ src/main/java/br/com/turnover/models/DepartamentoModel.java
@@ -2,12 +2,14 @@ package br.com.turnover.models;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
+import lombok.Data;
 
 import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.UUID;
 
+@Data
 @Entity
 @Table(name = "TB_DEPARTAMENTO")
 public class DepartamentoModel implements Serializable {
@@ -37,78 +39,6 @@ public class DepartamentoModel implements Serializable {
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     @OneToMany(mappedBy = "departamento", fetch = FetchType.LAZY)
     private Set<FuncionarioModel> funcionarios = new HashSet<>();
-
-    public UUID getId() {
-        return id;
-    }
-
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
-    public String getNomeDepartamento() {
-        return nomeDepartamento;
-    }
-
-    public void setNomeDepartamento(String nomeDepartamento) {
-        this.nomeDepartamento = nomeDepartamento;
-    }
-
-    public String getLocalizacao() {
-        return localizacao;
-    }
-
-    public void setLocalizacao(String localizacao) {
-        this.localizacao = localizacao;
-    }
-
-    public String getSenhaDepartamento() {
-        return senhaDepartamento;
-    }
-
-    public void setSenhaDepartamento(String senhaDepartamento) {
-        this.senhaDepartamento = senhaDepartamento;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-
-    public int getLotacaoMax() {
-        return lotacaoMax;
-    }
-
-    public void setLotacaoMax(int lotacaoMax) {
-        this.lotacaoMax = lotacaoMax;
-    }
-
-    public int getTurnoA() {
-        return turnoA;
-    }
-
-    public void setTurnoA(int turnoA) {
-        this.turnoA = turnoA;
-    }
-
-    public int getTurnoB() {
-        return turnoB;
-    }
-
-    public void setTurnoB(int turnoB) {
-        this.turnoB = turnoB;
-    }
-
-    public int getTurnoC() {
-        return turnoC;
-    }
-
-    public void setTurnoC(int turnoC) {
-        this.turnoC = turnoC;
-    }
 
     public Set<FuncionarioModel> getFuncionario() {
         Set<FuncionarioModel> funcionario = Set.of();
--- src/main/java/br/com/turnover/models/FuncionarioModel.java
+++ src/main/java/br/com/turnover/models/FuncionarioModel.java
@@ -1,12 +1,13 @@
 package br.com.turnover.models;
 
 import jakarta.persistence.*;
+import lombok.Data;
 
 import java.io.Serializable;
-import java.time.LocalDate;
 import java.util.Date;
 import java.util.UUID;
 
+@Data
 @Entity
 @Table(name = "TB_FUNCIONARIO")
 public class FuncionarioModel implements Serializable {
@@ -45,99 +46,5 @@ public class FuncionarioModel implements Serializable {
     @JoinColumn(name = "departamento_id")
     private DepartamentoModel departamento;
 
-    public UUID getId() {
-        return id;
-    }
 
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
-    public String getSenha() {
-        return senha;
-    }
-
-    public void setSenha(String senha) {
-        this.senha = senha;
-    }
-
-    public Date getNascimento() {
-        return nascimento;
-    }
-
-    public void setNascimento(Date nascimento) {
-        this.nascimento = nascimento;
-    }
-
-    public Date getContratacao() {
-        return contratacao;
-    }
-
-    public void setContratacao(Date contratacao) {
-        this.contratacao = contratacao;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public boolean isRemotoOuHibrido() {
-        return remotoOuHibrido;
-    }
-
-    public void setRemotoOuHibrido(boolean remotoOuHibrido) {
-        this.remotoOuHibrido = remotoOuHibrido;
-    }
-
-    public boolean isPontoDiarioEntrada() {
-        return pontoDiarioEntrada;
-    }
-
-    public void setPontoDiarioEntrada(boolean pontoDiarioEntrada) {
-        this.pontoDiarioEntrada = pontoDiarioEntrada;
-    }
-
-    public boolean isPontroDiarioSaida() {
-        return pontroDiarioSaida;
-    }
-
-    public void setPontroDiarioSaida(boolean pontroDiarioSaida) {
-        this.pontroDiarioSaida = pontroDiarioSaida;
-    }
-
-    public CargoModel getCargo() {
-        return cargo;
-    }
-
-    public void setCargo(CargoModel cargo) {
-        this.cargo = cargo;
-    }
-
-    public DepartamentoModel getDepartamento() {
-        return departamento;
-    }
-
-    public void setDepartamento(DepartamentoModel departamento) {
-        this.departamento = departamento;
-    }
 }
