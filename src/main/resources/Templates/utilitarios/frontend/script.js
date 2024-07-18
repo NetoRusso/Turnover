15c3aaa704bd9b8a7c7b522cef0259af6c1fa3d0
Okamiih <douglasdanielmf@live.com>
2024-07-18 00:17:19 -0300

    subindo base do front (douglas)

---
+++ src/main/resources/Templates/utilitarios/frontend/script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const navLinks = document.querySelector('.nav-links');
+
+    menuToggle.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+        menuToggle.classList.toggle('active');
+    });
+});
