44365fe8aa399f9fa0b53ac01f329921694f877f
Romulo Nogueira de Souza <romulonogueira929@gmail.com>
2024-07-21 02:51:24 -0300

    Ajuste fino do codigo

--- src/main/java/br/com/turnover/controller/AlocacaoController.java
+++ src/main/java/br/com/turnover/controller/AlocacaoController.java
@@ -1,35 +1,43 @@
 package br.com.turnover.controller;
 
 
-//@RestController
-//    @RequestMapping("/alocacoes")
-//    public class AlocacaoController {
-//        @Autowired
-//        private AlocacaoService alocacaoService;
-//
-//        @GetMapping
-//        public <Alocacao> List getAllAlocacoes() {
-//            return alocacaoService.findAll();
-//        }
-//
-//        @GetMapping("/{id}")
-//        public <Alocacao> Alocacao getAlocacaoById(@PathVariable Long id) {
-//            return (Alocacao) alocacaoService.findById(id);
-//        }
-//
-//        @PostMapping
-//        public <Alocacao> Alocacao createAlocacao(@RequestBody Alocacao alocacao) {
-//            return alocacaoService.save(alocacao);
-//        }
-//
-//        @PutMapping("/{id}")
-//        public <Alocacao> Alocacao updateAlocacao(@PathVariable Long id, @RequestBody Alocacao alocacao) throws InterruptedException {
-//            alocacao.wait(id);
-//            return alocacaoService.save(alocacao);
-//        }
-//
-//        @DeleteMapping("/{id}")
-//        public void deleteAlocacao(@PathVariable Long id) {
-//            alocacaoService.deleteById(id);
-//        }
+import br.com.turnover.dtos.AlocacaoRecordDto;
+import br.com.turnover.models.AlocacaoModel;
+import br.com.turnover.services.AlocacaoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/alocacoes")
+public class AlocacaoController {
+    @Autowired
+    private AlocacaoService alocacaoService;
+
+    @GetMapping
+    public List<AlocacaoModel> getAllAlocacoes() {
+        return alocacaoService.findAll();
+    }
+
+//    //@GetMapping("/{id}")
+//    public <Alocacao> Alocacao getAlocacaoById(@PathVariable Long id) {
+//        return (Alocacao) alocacaoService.findById(id);
 //    }
+
+    @PostMapping
+    public AlocacaoModel createAlocacao(@RequestBody AlocacaoRecordDto alocacao) {
+        return alocacaoService.save(alocacao);
+    }
+
+//    @PutMapping("/{id}")
+//    public <Alocacao> Alocacao updateAlocacao(@PathVariable Long id, @RequestBody Alocacao alocacao) throws InterruptedException {
+//        alocacao.wait(id);
+//        return alocacaoService.save(alocacao);
+//    }
+
+//    @DeleteMapping("/{id}")
+//    public void deleteAlocacao(@PathVariable Long id) {
+//        alocacaoService.deleteById(id);
+//    }
+}
--- src/main/java/br/com/turnover/services/AlocacaoService.java
+++ src/main/java/br/com/turnover/services/AlocacaoService.java
@@ -1,25 +1,43 @@
 package br.com.turnover.services;
 
-//@Service
-//public class AlocacaoService {
-//    @Autowired
-//    private AlocacaoRepository alocacaoRepository;
-//
-//    public List findAll() {
-//        return alocacaoRepository.findAll();
+import br.com.turnover.dtos.AlocacaoRecordDto;
+import br.com.turnover.models.AlocacaoModel;
+import br.com.turnover.models.FuncionarioModel;
+import br.com.turnover.repositories.AlocacaoRepository;
+import br.com.turnover.repositories.FuncionarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AlocacaoService {
+    @Autowired
+    private FuncionarioRepository FuncionarioRepository;
+    @Autowired
+    private AlocacaoRepository alocacaoRepository;
+
+    public List<AlocacaoModel> findAll() {
+        return alocacaoRepository.findAll();
+    }
+
+//   /public Object findById(Long id) {
+//       return alocacaoRepository.findById(id).orElse(null);
 //    }
-//
-//    public Object findById(Long id) {
-//        return alocacaoRepository.findById(id).orElse(null);
-//    }
-//
-//    public <Alocacao> Alocacao save(Alocacao alocacao) {
-//
-//        Alocacao save = (Alocacao) alocacaoRepository.save(alocacao);
-//        return save;
-//    }
-//
+
+    public AlocacaoModel save(AlocacaoRecordDto alocacao) {
+       FuncionarioModel funcionarioModel = FuncionarioRepository.findById(alocacao.funcionarioId()).orElse(null);
+        if (funcionarioModel == null) {
+            throw new RuntimeException("Funcionario não encontrado");
+        }
+            AlocacaoModel alocacaoModel = new AlocacaoModel();
+            alocacaoModel.setAlteracao(alocacao.alteracao());
+            alocacaoModel.setFuncionario(funcionarioModel);
+            return alocacaoRepository.save(alocacaoModel);
+
+    }
+
 //    public void deleteById(Long id) {
 //        alocacaoRepository.deleteById(id);
 //    }
-//}
+}
